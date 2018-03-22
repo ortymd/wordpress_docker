8029b942f253a11309bf881af692ab0451ee40b5
dmytro polyarush <polyarush.dmytro@gmail.com>
2018-03-22 12:41:25 +0000

    error 403 when adding --volume for html in wordpress container

--- start_wp_mysql.sh
+++ start_wp_mysql.sh
@@ -18,7 +18,6 @@ case $# in
 		;;
 esac
 
-
 BASE_PATH=$(pwd);
 database="$BASE_PATH/wordpress-data/database";
 html="$BASE_PATH/wordpress-data/html";
@@ -27,5 +26,6 @@ mkdir -p $database;
 mkdir -p $html;
 
 docker run -e MYSQL_ROOT_PASSWORD=123qwe -e MYSQL_USER=wpuser -e MYSQL_PASSWORD=wpuser -e MYSQL_DATABASE=wordpress_db \
-					 --name wordpressdb --detach mariadb
-docker run --name wordpress --publish 8080:80 --link wordpressdb:mysql --detach wordpress:4.9.3_nginx
+					 --volume $database:/var/lib/mysql --name wordpressdb --detach mariadb
+
+docker run --name wordpress --publish 8080:80 --volume $html:/var/www/html --link wordpressdb:mysql --detach wordpress:4.9.3_nginx
